c7389ed255be61fa28aee0eba96584413c70bf2f
Anton <abramov1991a@gmail.com>
2024-05-22 23:38:43 +0500

    Move data from template to view

--- places/models.py
+++ places/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 class Place(models.Model):
   title = models.CharField(max_length=200)
+  place_id = models.CharField(max_length=200, unique=True)
   description_short = models.TextField()
   description_long = models.TextField()
   lng = models.FloatField(max_length=200)
   lat = models.FloatField(max_length=200)
 
+  def __str__(self) -> str:
+    return self.title
 
 class Image(models.Model):
-  title = models.CharField(max_length=200)
+  title = models.ForeignKey(Place, on_delete=models.CASCADE)
   image = models.ImageField()
--- places/views.py
+++ places/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from places.models import Place
 
-# Create your views here.
+def show_index(request):
+    places = Place.objects.all()
+    for place in places:
+        features = [
+            {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lng, place.lat]
+                },
+            "properties": {
+                "title": place.title,
+                "placeId": place.place_id,
+                "detailsUrl": f"static/places/{place.place_id}.json"
+                }
+            }
+            ]
+
+
+    context = {
+        'places_geojson': {
+            "type": "FeatureCollection",
+            "features": features
+        }
+    }
+
+    return render(request, "index.html", context = context)
