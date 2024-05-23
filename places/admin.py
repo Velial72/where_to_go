ffe47d26f66d6cd6d0ee5c47908c2dccde93fe92
Anton <abramov1991a@gmail.com>
2024-05-23 22:49:29 +0500

    [200~add preview

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
 
 
 class AdminImageInline(admin.TabularInline):
     model = Image
     extra = 1
+    readonly_fields = ['preview_image']
+
+    def preview_image(self, obj):
+        if obj:
+            return format_html('<img src="{}" style="max-height: 200px; max-width: 100%;" />', obj.image.url)
+        else:
+            return 'картинки нет'
+        
+    preview_image.short_description = 'get preview'
 
 
 @admin.register(Place)
@@ -13,3 +23,4 @@ class AdminPlace(admin.ModelAdmin):
     inlines = [
         AdminImageInline
     ]
+    
