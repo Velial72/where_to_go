d297a8871c8de16e7b963807a120a14e8d7c765e
Velial72 <126498671+Velial72@users.noreply.github.com>
2024-06-12 20:29:37 +0500

    Update views.py
    кавычки

--- places/views.py
+++ places/views.py
@@ -10,15 +10,15 @@ def show_index(request):
     for place in places:
         features.append(
             {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [place.lng, place.lat]
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.lng, place.lat]
                 },
-            "properties": {
-                "title": place.title,
-                "placeId": place.title,
-                "detailsUrl": reverse('get_json_data', args=[place.id])
+            'properties': {
+                'title': place.title,
+                'placeId': place.title,
+                'detailsUrl': reverse('get_json_data', args=[place.id])
                 }
             }
         )
@@ -27,25 +27,25 @@ def show_index(request):
 
     context = {
         'places_geojson': {
-            "type": "FeatureCollection",
-            "features": features
+            'type': 'FeatureCollection',
+            'features': features
         }
     }
 
-    return render(request, "index.html", context = context)
+    return render(request, 'index.html', context = context)
 
 
 def get_json_data(request, place_id):
     place = get_object_or_404(Place.objects.prefetch_related('images'), pk=place_id)
 
     response = {
-        "title": place.title,
-        "imgs": [photo.image.url for photo in place.images.all()],
-        "description_short": place.description_short,
-        "description_long": place.description_long,
-        "coordinates": {
-            "lat": place.lat,
-            "lng": place.lng
+        'title': place.title,
+        'imgs': [photo.image.url for photo in place.images.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lat': place.lat,
+            'lng': place.lng
         }
     }
 
