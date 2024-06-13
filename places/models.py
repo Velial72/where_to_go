cacecc1fc2758a0ef17940736ce2a11cfe9af217
Velial72 <126498671+Velial72@users.noreply.github.com>
2024-06-13 10:29:30 +0500

    Update models.py

--- places/models.py
+++ places/models.py
@@ -3,31 +3,25 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-  title = models.CharField(max_length=200, verbose_name='название', unique=True)
-  description_short = models.TextField(verbose_name='короткое описание', blank=True)
-  description_long = HTMLField(verbose_name='описание', blank=True)
-  lng = models.FloatField(verbose_name='долгота')
-  lat = models.FloatField(verbose_name='широта')
+    title = models.CharField(max_length=200, verbose_name='название', unique=True)
+    description_short = models.TextField(verbose_name='короткое описание', blank=True)
+    description_long = HTMLField(verbose_name='описание', blank=True)
+    lng = models.FloatField(verbose_name='долгота')
+    lat = models.FloatField(verbose_name='широта')
 
-  class Meta:
-    verbose_name = 'место'
-    verbose_name_plural = 'места'
+    class Meta:
+        verbose_name = 'место'
+        verbose_name_plural = 'места'
 
-  def __str__(self) -> str:
-    return self.title
+    def __str__(self) -> str:
+        return self.title
   
 
 class Image(models.Model):
-  title = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='место', related_name='images')
-  image = models.ImageField(verbose_name='картинка', upload_to='')
-  position = models.IntegerField(verbose_name='позиция', db_index=True, default=0)
+    title = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='место', related_name='images')
+    image = models.ImageField(verbose_name='картинка', upload_to='')
+    position = models.IntegerField(verbose_name='позиция', db_index=True, default=0)
 
-  def save(self, *args, **kwargs):
-    if not self.position:
-        last_position = Image.objects.filter(title=self.title).aggregate(models.Max('position'))['position__max']
-        self.position = (last_position or 0) + 1
-    super().save(*args, **kwargs)
-    
   class Meta:
       ordering = ['position']
       verbose_name = 'картинка'
