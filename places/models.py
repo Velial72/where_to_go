c51b83ae4b7eb3325bcdebe1c7bfe0fafabce458
Velial72 <126498671+Velial72@users.noreply.github.com>
2024-06-14 18:01:17 +0500

    Update models.py

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='название', unique=True)
-    description_short = models.TextField(verbose_name='короткое описание', blank=True)
-    description_long = HTMLField(verbose_name='описание', blank=True)
+    short_description = models.TextField(verbose_name='короткое описание', blank=True)
+    long_description = HTMLField(verbose_name='описание', blank=True)
     lng = models.FloatField(verbose_name='долгота')
     lat = models.FloatField(verbose_name='широта')
 
